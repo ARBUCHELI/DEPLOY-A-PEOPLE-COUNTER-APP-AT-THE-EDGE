6de682b2966ea46d0a53a2e94b32ea55f7dcf689
Andrés R. Bucheli <radio_seattle@hotmail.com>
2020-06-09 02:32:51 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -25,7 +25,7 @@
 import os
 import sys
 import logging as log
-from openvino.inference_engine import IENetwork, IECore
+from openvino.inference_engine import IENetwork, IECore, IEPlugin
 
 
 class Network:
@@ -36,32 +36,81 @@ class Network:
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
-
-    def load_model(self):
+        self.plugin = None
+        self.net = None
+        self.input_blob = None
+        self.output_blob = None
+        self.net_plugin = None
+        self.infer_request_handle = None
+    
+    def load_model(self, model, device, inputz, outputz, num_requests, cpu_extension=None, plugin=None):
         ### TODO: Load the model ###
-        ### TODO: Check for supported layers ###
+        model_xml = model
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
+        if not plugin:
+            self.plugin = IEPlugin(device=device)
+        else:
+            self.plugin = plugin
         ### TODO: Add any necessary extensions ###
+        if cpu_extension and 'CPU' in device:
+            self.plugin.add_cpu_extension(cpu_extension)
+
+        self.net = IENetwork(model=model_xml, weights=model_bin)
+        ### TODO: Check for supported layers ###
+        if self.plugin.device == "CPU":
+            supported_layers = self.plugin.get_supported_layers(self.net)
+            unsupported_layers = [l for l in self.net.layers.keys() if l not in supported_layers]
+                
+            if len(unsupported_layers) != 0:
+                print("Unsupported layers found: {}".format(unsupported_layers))
+                print("Check whether extensions are available to add to IECore.")
+                sys.exit(1)
+              
         ### TODO: Return the loaded inference plugin ###
+        if num_requests == 0:
+            # Loads network read from IR to the plugin
+            self.net_plugin = self.plugin.load(network=self.net)
+        else:
+            self.net_plugin = self.plugin.load(network=self.net, num_requests=num_requests)
+        
+        self.input_blob = next(iter(self.net.inputs))
+        self.out_blob = next(iter(self.net.outputs))
+        assert len(self.net.inputs.keys()) == inputz, \
+            "Only able to support {} input topologies".format(len(self.net.inputs))
+        assert len(self.net.outputs) == outputz, \
+            "Only able to support {} output topologies".format(len(self.net.outputs))
         ### Note: You may need to update the function parameters. ###
-        return
+        return self.plugin, self.get_input_shape()
 
     def get_input_shape(self):
         ### TODO: Return the shape of the input layer ###
-        return
+        return self.net.inputs[self.input_blob].shape
 
-    def exec_net(self):
+    def exec_net(self, request_id, frame):
         ### TODO: Start an asynchronous request ###
+         self.infer_request_handle = self.net_plugin.start_async(
+            request_id=request_id, inputs={self.input_blob: frame})
         ### TODO: Return any necessary information ###
+         return self.net_plugin
         ### Note: You may need to update the function parameters. ###
-        return
 
-    def wait(self):
+    def wait(self, request_id):
         ### TODO: Wait for the request to be complete. ###
+        status = self.net_plugin.requests[request_id].wait(-1)
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        return status
 
-    def get_output(self):
+    def get_output(self, request_id, output=None):
         ### TODO: Extract and return the output results
         ### Note: You may need to update the function parameters. ###
-        return
+        if output:
+            res = self.infer_request_handle.outputs[output]
+        else:
+            res = self.net_plugin.requests[request_id].outputs[self.out_blob]
+        return res
+    
+    def clean(self):
+        del self.net_plugin
+        del self.plugin
+        del self.net
